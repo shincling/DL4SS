bbc40bf0d8c727899cc198f9bbc852ed30d103aa
shincling <shijing609@163.com>
2017-11-07 11:17:18 +0800

    各种bug之后，完成了mix_speech的建模，这个整体的类的设计还是需要考虑一下，有点乱现在。

--- Torch_multi/main_run.py
+++ Torch_multi/main_run.py
@@ -3,6 +3,7 @@ import sys
 import torch
 from torch import nn
 from torch.autograd import Variable
+import torch.nn.functional as F
 import numpy as np
 import time
 import config
@@ -14,10 +15,40 @@ np.random.seed(1)#设定种子
 # sys.stdout=log_file
 # logfile=config.LOG_FILE_PRE
 
+class MIX_SPEECH(nn.Module):
+    def __init__(self,input_fre,mix_speech_len):
+        super(MIX_SPEECH,self).__init__()
+        self.input_fre=input_fre
+        self.mix_speech_len=mix_speech_len
+        self.layer=nn.LSTM(
+            input_size=input_fre,
+            hidden_size=config.HIDDEN_UNITS,
+            num_layers=config.NUM_LAYERS,
+            batch_first=True,
+            bidirectional=True
+        )
+        self.Linear=nn.Linear(2*config.HIDDEN_UNITS,self.input_fre*config.EMBEDDING_SIZE)
+
+    def forward(self,x):
+        x,hidden=self.layer(x)
+        x=x.contiguous()
+        x=x.view(config.BATCH_SIZE*self.mix_speech_len,-1)
+        out=F.tanh(self.Linear(x))
+        out=out.view(config.BATCH_SIZE,self.mix_speech_len,self.input_fre,-1)
+        print out.size()
+        return out
+
+
 class MULTI_MODAL(object):
-    def __init__(self,datasize):
+    def __init__(self,datasize,gen):
         print 'Begin to build the maim model for Multi_Modal Cocktail Problem.'
         self.mix_speech_len,self.speech_fre,self.total_frames,self.spk_num_total=datasize
+        self.gen=gen
+
+    def build(self):
+        mix_hidden_layer_3d=MIX_SPEECH(self.speech_fre,self.mix_speech_len)
+        output=mix_hidden_layer_3d(Variable(torch.from_numpy(self.gen.next()[1])))
+
 
 def main():
     print('go to model')
@@ -29,8 +60,8 @@ def main():
     #此处顺序是 mix_speechs.shape,mix_feas.shape,aim_fea.shape,aim_spkid.shape,query.shape
     #一个例子：(5, 17040) (5, 134, 129) (5, 134, 129) (5,) (5, 32, 400, 300, 3)
     datasize=prepare_datasize(data_generator)
-    multi_model=MULTI_MODAL(datasize)
-
+    multi_model=MULTI_MODAL(datasize,data_generator)
+    multi_model.build()
 
 
 
