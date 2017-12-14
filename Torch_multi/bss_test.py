e406c901cbb7ab4171ef0c8885755479e3732ea5
shincling <shijing609@163.com>
2017-12-14 10:40:17 +0800

    额外加了个静默音来筛选top通道

--- Torch_multi/bss_test.py
+++ Torch_multi/bss_test.py
@@ -6,6 +6,7 @@ from separation import bss_eval_sources
 
 path='server_out/'
 aim_mix_number=2 #只筛选具有这个个数的混合语音（只计算有俩人合成的）
+add_slience_channel=1
 def cal(path,aim_mix_number):
     mix_number=len(set([l.split('_')[0] for l in os.listdir(path) if l[-3:]=='wav']))
     print 'num of mixed :',mix_number
@@ -20,6 +21,7 @@ def cal(path,aim_mix_number):
             if l[-3:]!='wav':
                 continue
             if l.split('_')[0]==str(idx):
+                # print l
                 if 'True_mix' in l:
                     mix_speech.append(sf.read(path+l)[0])
                 if 'genTrue' in l:
@@ -27,19 +29,29 @@ def cal(path,aim_mix_number):
                 if 'pre' in l:
                     pre_speech_channel.append(sf.read(path+l)[0])
 
-        assert len(aim_speech_channel)==len(pre_speech_channel)
+        # assert len(aim_speech_channel)==len(pre_speech_channel)
         if len(aim_speech_channel)!=aim_mix_number:
             continue
         aim_speech_channel=np.array(aim_speech_channel)
         pre_speech_channel=np.array(pre_speech_channel)
         # print aim_speech_channel.shape
         # print pre_speech_channel.shape
-        # result=bss_eval_sources(aim_speech_channel,pre_speech_channel)
-        result=bss_eval_sources(aim_speech_channel,aim_speech_channel)
-        # print result
+        # result=bss_eval_sources(aim_speech_channel,pre_speech_channel,False)
+        # permu=bss_eval_sources(aim_speech_channel,pre_speech_channel)[-1]
+        # print permu
+        # result=bss_eval_sources(aim_speech_channel[[1,0]],pre_speech_channel)
+        if add_slience_channel:
+            num_add_cha=pre_speech_channel.shape[0]-aim_speech_channel.shape[0]
+            aim_speech_channel_add=np.concatenate((aim_speech_channel,np.zeros([num_add_cha,pre_speech_channel.shape[1]])+1e-5))
+            permu=bss_eval_sources(aim_speech_channel_add,pre_speech_channel)[-1][:aim_mix_number]
+            result=bss_eval_sources(aim_speech_channel,pre_speech_channel[[permu]])
+        else:
+            result=bss_eval_sources(aim_speech_channel,pre_speech_channel)
+
+        print result
         SDR_sum=np.append(SDR_sum,result[0])
     print SDR_sum.mean()
     return SDR_sum
 
-
+cal(path,aim_mix_number)
 
