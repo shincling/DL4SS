1e6bd7634463454c6a34a4a09bcd31880f181f41
shincling <shijing609@163.com>
2018-02-27 15:00:07 +0800

    add two examples about the repeated predition problem.

--- Torch_multi/analyze_data.py
+++ Torch_multi/analyze_data.py
@@ -5,7 +5,8 @@ from sklearn.decomposition.pca import PCA
 import matplotlib
 import matplotlib.pyplot as plt
 
-cc=torch.load('params/param_mix101_WSJ0_emblayer_180')
+# cc=torch.load('params/param_mix101_WSJ0_emblayer_180')
+cc=torch.load('params/param_mix2_db2dropout_WSJ0_emblayer_180')
 cc=cc['layer.weight'].cpu().numpy()
 pca=PCA(2)
 newData=pca.fit_transform(cc)
@@ -13,5 +14,7 @@ print newData.shape
 f1=plt.figure(1)
 for data in newData:
     plt.scatter(newData[:,0],newData[:,1])
+plt.scatter(0,-2.12,c='r',marker='D')
+plt.scatter(2.02,0.47,c='r',marker='D')
 plt.show()
 
